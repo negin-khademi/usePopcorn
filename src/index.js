880bf1232ffeab3096647e3d5e27fa5c449b0690
Negin <negin.khademi@gmail.com>
2024-10-20 16:04:36 +0330

    improving reusability with props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 // import './index.css';
 
 // import App from './App';
-import React from 'react';
+import React, { useState } from 'react';
+
 import ReactDOM from 'react-dom/client';
 import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} star</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {
       /*
-  <App /> */ <>
-        <StarRating maxRating={5} />
+  <App /> */
+      <>
+        <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okey', 'Good', 'Amasing']} />
+        <StarRating size={24} color="red" defaultRating={3} />
+        <Test />
       </>
     }
   </React.StrictMode>
