77dee9c1d001b507087fd5430c8cefa5b892b365
Negin <negin.khademi@gmail.com>
2024-12-01 15:54:10 +0330

    clean up data fetching

--- src/App.js
+++ src/App.js
@@ -78,20 +78,26 @@ export default function App() {
   useEffect(
     function () {
       async function fetchMovie() {
+        const controller = new AbortController();
         try {
           setIsLoading(true);
           setError('');
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
 
           if (!res.ok) throw new Error('Something went wrong with fetching movies');
 
           const data = await res.json();
 
           if (data.Response === 'False') throw new Error('Movie not found');
+
           setMovies(data.Search);
+          setError('');
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
